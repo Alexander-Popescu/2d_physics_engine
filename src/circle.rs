792155cbc4062a55eccf00ad48b315dcc7f18a2d
Alexander-Popescu <AlexanderPopescu5381@gmail.com>
2023-03-31 13:23:36 -0400

    added gravity and random velocity bumps

--- src/circle.rs
+++ src/circle.rs
@@ -24,11 +24,36 @@ impl Circle {
 
 
     //update the circle
-    pub fn update(&mut self, dt: f32, friction: &f32) {
+    pub fn update(&mut self, dt: f32, friction: f32, width: f32, height: f32, gravity: f32, restitution: f32) {
+        //append gravity to y acceleration
+        self.acceleration.y += gravity;
         self.velocity += self.acceleration * dt;
         self.position += self.velocity * dt;
-        //add a vector to the velocity vector of opposite direction and magnitude friction
-        self.velocity += self.velocity * -1.0 * *friction * dt;
+
+        //invert the velocity if there is a collision with the edge of the screen
+        if self.position.x + self.radius > width {
+            //add a vector to the velocity vector of opposite direction and magnitude friction
+            self.velocity += self.velocity * -1.0 * friction * dt;
+            self.position.x = width - self.radius;
+            self.velocity.x *= -1.0 * restitution;
+            
+        } else if self.position.x - self.radius < 0.0 {
+            //add a vector to the velocity vector of opposite direction and magnitude friction
+            self.velocity += self.velocity * -1.0 * friction * dt;
+            self.position.x = self.radius;
+            self.velocity.x *= -1.0 * restitution;
+        }
+        if self.position.y + self.radius > height {
+            //add a vector to the velocity vector of opposite direction and magnitude friction
+            self.velocity += self.velocity * -1.0 * friction * dt;
+            self.position.y = height - self.radius;
+            self.velocity.y *= -1.0 * restitution;
+        } else if self.position.y - self.radius < 0.0 {
+            //add a vector to the velocity vector of opposite direction and magnitude friction
+            self.velocity += self.velocity * -1.0 * friction * dt;
+            self.position.y = self.radius;
+            self.velocity.y *= -1.0 * restitution;
+        }
         
     }
 
